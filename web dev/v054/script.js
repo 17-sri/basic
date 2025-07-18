4b27a446f75cda717b1ba48f089a6a3c032acd32
srikanth <srikanthv1709@gmail.com>
2025-07-18 19:12:51 +0530

    push

---
+++ web dev/v054/script.js
@@ -0,0 +1,14 @@
+alert("Hello world")
+console.log("Code is running")
+console.log("Code is again running ")
+var a = prompt("Enter your number")
+var isTrue = confirm("Are you sure to blast your computer")
+if(isTrue){
+    console.log("Your Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}
+console.log("your number is "+ a)
+document.title="By Javascript"
+document.body.style.backgroundColor = "gray"
